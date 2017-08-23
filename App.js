463c7d8693c5e45ce1484c9f159fe366dfd12005
isaac luis mori <luismorith@gmail.com>
2017-08-23 03:25:10 -0500

    refactoring files

--- App.js
+++ App.js
@@ -1,38 +1,4 @@
 import React from 'react';
-import { StackNavigator } from 'react-navigation';
-
-import MainScreen from './src/Containers/MainScreen.js';
-import ScheduleDetails from './src/Containers/ScheduleDetails.js';
-import SpeakerDetails from './src/Containers/SpeakerDetails.js';
-import JuntaScreen from './src/Containers/JuntaScreen.js';
-
-import HeaderSection from './src/Components/Header.js'
-
-
-const App = StackNavigator({
-  // Get all the tabas
-  MainScreen: {
-    screen: MainScreen,
-    navigationOptions: {
-      header: null
-    }
-  },
-  // after that we have to map all detail pages
-  schedule_detail: {
-    screen: ScheduleDetails,
-  },
-  speaker_details: {
-    screen: SpeakerDetails,
-  },
-  Junta: {
-    screen: JuntaScreen,
-  },
-}
-// , {initialRouteName: 'Junta'}
-);
+import App from './src/Containers/FullPages';
 
 export default App;
-
-// <View style={{paddingTop: Platform.OS === ‘ios’ ? 0 : Expo.Constants.statusBarHeight}}>
-//   <-- your react navigation stack or whatever goes here -->
-// </View>
